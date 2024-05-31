a0b142fb94325fae8d4a068437d74779041d508d
root <kimjoyc6@gmail.com>
2024-05-31 21:16:12 +0000

    color and debug flag

--- mdimechanic/cmd_run.py
+++ mdimechanic/cmd_run.py
@@ -4,17 +4,17 @@ import shutil
 from .utils.utils import format_return, insert_list, docker_error, get_mdi_standard, get_compose_path, get_package_path, get_mdimechanic_yaml, write_as_bytes
 from .utils.determine_compose import COMPOSE_COMMAND
 
-def run( script_name, base_path ):
-    mdimechanic_yaml = get_mdimechanic_yaml( base_path )
+def run(script_name, base_path, debug=False):
+    mdimechanic_yaml = get_mdimechanic_yaml(base_path)
 
     # Get the path to the docker-compose file
-    docker_path = os.path.join( base_path, ".mdimechanic", ".temp" )
+    docker_path = os.path.join(base_path, ".mdimechanic", ".temp")
 
     # Get a list of all containers in this calculation
-    containers = [ key for key in mdimechanic_yaml['run_scripts'][script_name]['containers'] ]
+    containers = [key for key in mdimechanic_yaml['run_scripts'][script_name]['containers']]
 
     # Create the docker-compose.yml file
-    docker_compose_text='''version: '3'
+    docker_compose_text = '''version: '3'
 
 services:
 '''
@@ -24,7 +24,7 @@ services:
 
         container_yaml = mdimechanic_yaml['run_scripts'][script_name]['containers'][containers[icontainer]]
 
-        if not 'image' in container_yaml:
+        if 'image' not in container_yaml:
             raise Exception("No image was provided for container \"" + str(containers[icontainer]) + "\".  Please provide the image name in mdimechanic.yml.")
 
         image_name = container_yaml['image']
@@ -49,7 +49,7 @@ services:
 '''.format(**textargs)
 
         if 'gpu' in mdimechanic_yaml['docker']:
-                docker_compose_text += '''deploy:
+            docker_compose_text += '''deploy:
       resources:
         reservations:
           devices:
@@ -64,13 +64,12 @@ networks:
     driver: "bridge"
 '''
 
-    docker_compose_path = os.path.join( base_path, ".mdimechanic", ".temp", "docker-compose.yml" )
+    docker_compose_path = os.path.join(base_path, ".mdimechanic", ".temp", "docker-compose.yml")
     os.makedirs(os.path.dirname(docker_compose_path), exist_ok=True)
-    write_as_bytes( docker_compose_text, docker_compose_path )
+    write_as_bytes(docker_compose_text, docker_compose_path)
 
     # Write the run script for each of the engines
     for icontainer in range(len(containers)):
-
         container_yaml = mdimechanic_yaml['run_scripts'][script_name]['containers'][containers[icontainer]]
 
         # Write the run script for the engine
@@ -82,65 +81,44 @@ if [ ! -d "/repo" ]; then
     ln -s /mdi_shared /repo
 fi
 '''
-        #script = "#!/bin/bash\nset -e\ncd /mdi_shared\n"
-        #script += "export MDI_OPTIONS=\'-role ENGINE -name TESTCODE -method TCP -hostname mdi_mechanic -port 8021\'\n"
         for line in script_lines:
             script += line + '\n'
 
         # Write the script to run the test
         script_file_name = "docker_mdi_" + str(icontainer) + ".sh"
-        script_path = os.path.join( base_path, ".mdimechanic", ".temp", script_file_name )
+        script_path = os.path.join(base_path, ".mdimechanic", ".temp", script_file_name)
         os.makedirs(os.path.dirname(script_path), exist_ok=True)
-        write_as_bytes( script, script_path )
+        write_as_bytes(script, script_path)
 
     # Launch with docker-compose
     docker_env = os.environ
-    up_proc = subprocess.Popen(COMPOSE_COMMAND + ["up"],
+    up_proc = subprocess.Popen(['unbuffer'] + COMPOSE_COMMAND + ["up"],
                                stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                               cwd=docker_path, env=docker_env, text=True)
+                               cwd=docker_path, env=docker_env, 
+                                encoding='utf-8')
 
-  # Print output in real-time
-    while up_proc.poll() is None:
-        line = up_proc.stdout.readline()
-        if line:
-            print(line, end='')  # Print each line as it's available
-
-    # Print remaining output
-    up_out, up_err = up_proc.communicate()
-    if up_out:
-        print(up_out)
-    if up_err:
-        print(up_err)
+    if debug:
+        # Print output in real-time
+        while up_proc.poll() is None:
+            up_out_line = up_proc.stdout.readline()
+            up_err_line = up_proc.stderr.readline()
+            if up_out_line:
+                print(up_out_line, end='')
+            if up_err_line:
+                print(up_err_line, end='')
 
     # Run "docker-compose down"
-    down_proc = subprocess.Popen(COMPOSE_COMMAND + ["down"],
+    down_proc = subprocess.Popen(['unbuffer'] + COMPOSE_COMMAND + ["down"],
                                  stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                                 cwd=docker_path, env=docker_env, text=True)
+                                 cwd=docker_path, env=docker_env, 
+                                 encoding='utf-8')
 
-    # Print output in real-time
-    while down_proc.poll() is None:
-        line = down_proc.stdout.readline()
-        if line:
-            print(line, end='')  # Print each line as it's available
-
-    # Print remaining output
-    down_out, down_err = down_proc.communicate()
-    if down_out:
-        print(down_out)
-    if down_err:
-        print(down_err)
-
-    if up_proc.returncode != 0:
-        docker_error((up_out, up_err), "Driver test returned non-zero exit code.")
-
-    elif down_proc.returncode != 0:
-        docker_error((down_out, down_err), "Driver test returned non-zero exit code on docker down.")
-
-    else:
-        print("====================================================")
-        print("================ Output from Docker ================")
-        print("====================================================")
-        print(up_out)
-        print("====================================================")
-        print("============== End Output from Docker ==============")
-        print("====================================================")
+    if debug:
+        # Print output in real-time
+        while down_proc.poll() is None:
+            down_out_line = down_proc.stdout.readline()
+            down_err_line = down_proc.stderr.readline()
+            if down_out_line:
+                print(down_out_line, end='')
+            if down_err_line:
+                print(down_err_line, end='')
